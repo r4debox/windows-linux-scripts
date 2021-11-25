e8812f2da93a2ceb884cb2e1999e180582f03804
r4debox <32275233+r4debox@users.noreply.github.com>
2021-11-25 12:41:10 -0800

    fix windows time when switching from linux to windows
    fix windows time when switching from linux to windows

---
+++ wintime.bat
@@ -0,0 +1,12 @@
+net file 1>NUL 2>NUL
+if not '%errorlevel%' == '0' (
+    powershell Start-Process -FilePath "%0" -ArgumentList "%cd%" -verb runas >NUL 2>&1
+    exit /b
+)
+cd /d %1
+net stop w32time
+w32tm /unregister
+w32tm /register
+net start w32time
+w32tm /resync
+pause
